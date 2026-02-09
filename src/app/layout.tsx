942cb00f46f4bb126861442faa9703926e769eef
ThanSoe <89002719+ThanSoe16@users.noreply.github.com>
2026-02-09 20:11:04 +0700

    icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,45 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Habit Tracker",
-  description: "Track your habits and improve your life",
+  title: {
+    default: process.env.NEXT_PUBLIC_APP_NAME!,
+    template: `%s | ${process.env.NEXT_PUBLIC_APP_NAME}`,
+  },
+  description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+  manifest: "/manifest.json",
+
+  openGraph: {
+    type: "website",
+    title: process.env.NEXT_PUBLIC_APP_NAME,
+    description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+    url: process.env.NEXT_PUBLIC_APP_URL,
+    siteName: process.env.NEXT_PUBLIC_APP_NAME,
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_APP_URL}/cover-image.png`,
+        width: 1200,
+        height: 630,
+        alt: process.env.NEXT_PUBLIC_APP_NAME,
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: process.env.NEXT_PUBLIC_APP_NAME,
+    description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+    images: [`${process.env.NEXT_PUBLIC_APP_URL}/cover-image.png`],
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+  },
+
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: process.env.NEXT_PUBLIC_APP_NAME,
+  },
 };
 
 export default function RootLayout({
